9f58f36e92efbc84c869ccbda25ac3ca69a06365
Ildar <Ildar@ILZAR-PC>
2025-11-01 16:53:15 +0500

    Добавьте файлы проекта.

---
+++ Zaripov12/class/IcalculatorBasic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CalculatorBasic.BasicCalculator.classes
+{
+        public class BasicCalculator : IcalculatorBasic
+        {
+            public double Addition(double value1, double value2)
+            {
+                return value1 + value2;
+            }
+
+            public double Divetion(double value1, double value2)
+            {
+                return value1 / value2;
+            }
+            public double Multiplication(double value1, double value2)
+            {
+                return value1 * value2;
+            }
+            public double Substraction(double value1, double value2)
+            {
+                return value1 - value2;
+            }
+        }
+}
---
+++ Zaripov12/class/IcalculatorBasic1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculatorBasic.BasicCalculator.classes;
+
+public class IcalculatorBasic
+{
+    public IcalculatorBasic()
+    { IcalculatorBasic _Bcalc = new BasicCalculator();
+        while (true)
+        {
+            Console.WriteLine("you have use this app?");
+            Console.WriteLine(" input yes/no");
+            String answer = Console.ReadLine().Trim().ToLower();
+            if ( answer == "yes")
+            {
+             int  First_number = Console.ReadLine("enter a number") 
+            }
+    }
+
+
+    }
+
+}
