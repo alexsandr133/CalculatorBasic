3da05ef5313afd62da3f95eff66deca236d6899e
alexsandr133 <alexsandramg65@gmail.com>
2025-11-13 18:29:29 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApp2_zaripov122
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+         
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip4 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.navigationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.photosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jobOpeningsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.numbersPhoneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proffilleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.signToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.signInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.delleteAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navigationToolStripMenuItem,
+            this.jobOpeningsToolStripMenuItem,
+            this.proffilleToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 3;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip4
+            // 
+            this.contextMenuStrip4.Name = "contextMenuStrip4";
+            this.contextMenuStrip4.Size = new System.Drawing.Size(61, 4);
+            // 
+            // navigationToolStripMenuItem
+            // 
+            this.navigationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.photosToolStripMenuItem,
+            this.dToolStripMenuItem,
+            this.contactsToolStripMenuItem});
+            this.navigationToolStripMenuItem.Name = "navigationToolStripMenuItem";
+            this.navigationToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.navigationToolStripMenuItem.Text = "navigation";
+            // 
+            // photosToolStripMenuItem
+            // 
+            this.photosToolStripMenuItem.Name = "photosToolStripMenuItem";
+            this.photosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.photosToolStripMenuItem.Text = "photos";
+            this.photosToolStripMenuItem.Click += new System.EventHandler(this.Loading_form2);
+            // 
+            // dToolStripMenuItem
+            // 
+            this.dToolStripMenuItem.Name = "dToolStripMenuItem";
+            this.dToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dToolStripMenuItem.Text = "downloads";
+            // 
+            // contactsToolStripMenuItem
+            // 
+            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
+            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.contactsToolStripMenuItem.Text = "contacts";
+            // 
+            // jobOpeningsToolStripMenuItem
+            // 
+            this.jobOpeningsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactsToolStripMenuItem1,
+            this.numbersPhoneToolStripMenuItem,
+            this.importantToolStripMenuItem});
+            this.jobOpeningsToolStripMenuItem.Name = "jobOpeningsToolStripMenuItem";
+            this.jobOpeningsToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.jobOpeningsToolStripMenuItem.Text = "job openings";
+            // 
+            // contactsToolStripMenuItem1
+            // 
+            this.contactsToolStripMenuItem1.Name = "contactsToolStripMenuItem1";
+            this.contactsToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.contactsToolStripMenuItem1.Text = "contacts";
+            // 
+            // numbersPhoneToolStripMenuItem
+            // 
+            this.numbersPhoneToolStripMenuItem.Name = "numbersPhoneToolStripMenuItem";
+            this.numbersPhoneToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.numbersPhoneToolStripMenuItem.Text = "numbers phone";
+            // 
+            // importantToolStripMenuItem
+            // 
+            this.importantToolStripMenuItem.Name = "importantToolStripMenuItem";
+            this.importantToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importantToolStripMenuItem.Text = "important";
+            // 
+            // proffilleToolStripMenuItem
+            // 
+            this.proffilleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.signToolStripMenuItem,
+            this.signInToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.delleteAccountToolStripMenuItem});
+            this.proffilleToolStripMenuItem.Name = "proffilleToolStripMenuItem";
+            this.proffilleToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.proffilleToolStripMenuItem.Text = "proffille";
+            // 
+            // signToolStripMenuItem
+            // 
+            this.signToolStripMenuItem.Name = "signToolStripMenuItem";
+            this.signToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.signToolStripMenuItem.Text = "register";
+            // 
+            // signInToolStripMenuItem
+            // 
+            this.signInToolStripMenuItem.Name = "signInToolStripMenuItem";
+            this.signInToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.signInToolStripMenuItem.Text = "sign in";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            // 
+            // delleteAccountToolStripMenuItem
+            // 
+            this.delleteAccountToolStripMenuItem.Name = "delleteAccountToolStripMenuItem";
+            this.delleteAccountToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.delleteAccountToolStripMenuItem.Text = "dellete account";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.loading_form);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip4;
+        private System.Windows.Forms.ToolStripMenuItem navigationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem photosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jobOpeningsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem numbersPhoneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importantToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proffilleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem signToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem signInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem delleteAccountToolStripMenuItem;
+    }
+}
