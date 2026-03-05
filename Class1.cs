49ec0717ff838b401e7caf76df533947edfd26e6
alexsandr133 <alexsandramg65@gmail.com>
2026-03-05 13:26:45 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public int RectangleArea(int a, int b)
+	    {
+		return a * b;
+      	}
+	}
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class GeometryTest
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+             //исходные 
+        }
+    }
+}
