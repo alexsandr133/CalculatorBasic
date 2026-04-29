68f98e3d71253575f1c94ebfe3d36abbbc496053
alexsandr133 <alexsandramg65@gmail.com>
2026-04-29 14:48:40 +0500

    Add files via upload

---
+++ zaripov56/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+StatusOrder zakaz1;
+zakaz1.name = "бмв е34";
+struct StatusOrder
+{   
+    string name;
+    int id;
+    decimal price;
+    int quantity;
+    int OrderStatus;
+    
+    public StatusOrder(string name,int id, decimal price,  int OrderStatus)
+    {   
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.OrderStatus = OrderStatus;
+    }
+    public double ReturnStatus()
+    {
+        switch(OrderStatus)
+        {
+            case (1):
+            Console.WriteLine("заказ оформлен");
+            break;
+            default:
+            case (2):
+            Console.WriteLine("заказ отменен");
+            break;
+        }
+        Console.WriteLine("информация о заказе: "+" "+$"{id}"+" "+"его цена"+" "+$"{price}"+" "+"его cтатус"+" "+$"{OrderStatus}");
+        return OrderStatus;
+        
+    }
+}
+enum Status
+{
+  zakaz_placed= 1,
+   zakaz_cancel = 2
+   
+}
