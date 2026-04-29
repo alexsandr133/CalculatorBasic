dfd7834a1a7a8020c2f22df27c1af77101ceee90
alexsandr133 <alexsandramg65@gmail.com>
2026-04-29 11:17:42 +0500

    Add files via upload

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Product burger;
+burger.price = 50;
+burger.name_products = "vopper";
+burger.kolichestvo = 2;
+
+int result = burger.returnPrice();
+Console.WriteLine(result);
+
+
+//создаем структуру
+struct Product
+{
+    public int price;
+    public string name_products;
+    public int kolichestvo;
+
+    public Product(string name_products, int kolichestvo, int price)
+    {
+        this.name_products = name_products;
+        this.price = price;
+        this.kolichestvo = kolichestvo;
+    }
+    public int returnPrice() 
+    {
+        int result = price * kolichestvo;
+        Console.WriteLine("У вас товара на сумму: " + " " + $"{result}"+" " + "название продукта" + " " + $"{name_products}" +" "+ "его количество" + " " + $"{kolichestvo}");
+        return result;
+
+    }
+
+
+}
