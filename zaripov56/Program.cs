d724a6bdb1a9e30ac6254cb07fc48e35aeed3d23
alexsandr133 <alexsandramg65@gmail.com>
2026-04-30 12:54:49 +0500

    Add files via upload

--- zaripov56/Program.cs
+++ zaripov56/Program.cs
@@ -1,42 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices.Marshalling;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
 
-StatusOrder zakaz1;
-zakaz1.name = "бмв е34";
-struct StatusOrder
-{   
-    string name;
-    int id;
-    decimal price;
-    int quantity;
-    int OrderStatus;
-    
-    public StatusOrder(string name,int id, decimal price,  int OrderStatus)
-    {   
-        this.name = name;
-        this.id = id;
-        this.price = price;
-        this.OrderStatus = OrderStatus;
-    }
-    public double ReturnStatus()
-    {
-        switch(OrderStatus)
-        {
-            case (1):
-            Console.WriteLine("заказ оформлен");
-            break;
-            default:
-            case (2):
-            Console.WriteLine("заказ отменен");
-            break;
-        }
-        Console.WriteLine("информация о заказе: "+" "+$"{id}"+" "+"его цена"+" "+$"{price}"+" "+"его cтатус"+" "+$"{OrderStatus}");
-        return OrderStatus;
-        
-    }
-}
-enum Status
+
+
+
+
+InventoryManager Tovar="bmw";
+Tovar.name_tovar = "bmw m5";
+Tovar.id = 1;
+Tovar.numberTovar = 1;
+Tovar.StatusOrder = true;
+
+//класс PublicInventory
+struct InventoryManager
 {
-  zakaz_placed= 1,
-   zakaz_cancel = 2
-   
+    int number_tovar;
+    int id;
+    string name_tovar;
+    int kolichestvo;
+    bool StatusOrder;
+    public InventoryManager(int number_tovar,int id, string name_tovar, int kolichestvo, bool StatusOrder)
+    {   
+        this.number_tovar = number_tovar;
+        this.id = id;
+        this.name_tovar = name_tovar;
+        this.kolichestvo = kolichestvo;
+        this.StatusOrder = StatusOrder;
+    }
+    public int addTovars()
+    {
+        int places_shop = 100000;
+        
+        if (kolichestvo > places_shop)
+        {
+            Console.WriteLine("ВЫ добавили  товар  на склад");
+             Console.WriteLine("товар и информация о нем: "+" "+$"{name_tovar}"+" "+"его id"+" "+$"{id}"+" "+$"{number_tovar}");
+             
+
+        }
+        else
+        {
+             Console.WriteLine("мест нет");
+        }
+    }
+    public int removeTovar()
+    {   
+        var number_tovar = 0 ;
+        int places_shop = 100000;
+        int total =  places_shop - number_tovar; 
+        return total;
+        Console.WriteLine("склад и информация о нем: "+" "+$"{name_tovar}"+" "+" "+$"{number_tovar}"+" "+$"{places_shop}");
+    }
+    public void ReturnInformation()
+    {
+         
+         Console.WriteLine("товар и информация о нем: "+" "+$"{name_tovar}"+" "+"его id"+" "+$"{id}"+" "+$"{number_tovar}");
+    }
+
+
 }
+
