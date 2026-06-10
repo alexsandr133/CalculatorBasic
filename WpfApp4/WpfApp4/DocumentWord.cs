a72be756154c605d3f87319faf6d51bf477503c3
alexsandr133 <alexsandramg65@gmail.com>
2026-06-10 13:58:18 +0500

    Add files via upload

---
+++ WpfApp4/WpfApp4/DocumentWord.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+namespace WpfApp4
+{
+    internal class DocumentWord
+    {
+        //создаем обьект класса приложения
+        private Word.Application wordApp;
+        public DocumentWord()
+        {
+            try
+            {
+                wordApp = new Word.Application();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void OpenDoc(string name)
+        {
+            try
+            {
+                object fileName = name;
+                wordApp.Documents.Open(ref fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void AddDoc(string newName)
+        {
+            try
+            {
+                Word.Document doc = wordApp.Documents.Add();
+                object fileName = newName;
+                doc.SaveAs2(ref fileName);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void AddDoc(string template,string newName)
+        {
+            try
+            {
+                object templateFile = template;
+                Word.Document doc = wordApp.Documents.Add();
+                object fileName = newName;
+                doc.SaveAs2(ref fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void CloseDoc()
+        {
+            try
+            {
+                object saveChanges = Word.WdSaveOptions.wdPromptToSaveChanges;
+
+                wordApp.Quit(ref saveChanges);
+                wordApp = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+            public void Replace(string stubToPeplace,string text,string nameDoc)
+            {
+            try
+            {
+                Word.Document doc = wordApp.Documents[nameDoc];
+                bool found;
+                do
+                {
+                    var range = doc.Content;
+                    found = range.Find.Execute(
+                     FindText: stubToPeplace,
+                     ReplaceWith: text);
+                } while (found);
+                    doc.Save();         
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    }
+
---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DocumentWord word;
+        public MainWindow()
+        {
+            InitializeComponent();
+            word = new DocumentWord();
+        }
+
+        private void btnCreate_click(object sender, RoutedEventArgs e)
+        {
+            word.AddDoc(@"С:\Temp\Test.docx");
+            MessageBox.Show("документ создан");
+        }
+
+        private void btnOpen_click(object sender, RoutedEventArgs e)
+        {
+            word.OpenDoc(@"C:\Temp\Test.docx");
+            MessageBox.Show("документ открыт");
+        }
+
+        
+    }
+}
