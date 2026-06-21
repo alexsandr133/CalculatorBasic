b886aba52bad484ddc9a192301d2f01afe6a1deb
alexsandr133 <alexsandramg65@gmail.com>
2026-06-21 23:34:43 +0500

    Add files via upload

---
+++ Новая папка/Form2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
+using System.Diagnostics.Eventing.Reader;
+using System.Web;
+namespace Journal
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void exit4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //функция обработки нажатия для создания ока проводника
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            Keys modifierKeys = Control.ModifierKeys;
+            //проверяем какой модефикатор нажат
+            //можефикатор для кнопки отправить файл
+            if (ModifierKeys == Keys.Control)
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                
+                
+            }
+            else 
+            {
+                //object Message = System.Windows.Forms.Message.("вы ничего не нажали");
+            }
+
+        }
+        
+
+        private void  button1_Click(object sender, EventArgs e)
+        {
+            //указываем путь к файлу
+            string path = @"C:\users\Ilzar_LG_Gram\sourse\repos\students";
+           
+
+
+            //используем расщирение для окна проводника
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                //получаем путь к выбранному файлу
+                string FilePath = openFileDialog.FileName;
+                //фильтрация файлов
+                openFileDialog.Filter = "текстовые файлы (*.txt)|*.txt";
+                //по умолчанию первый фильтр
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.Title = "выберите тестовый файл";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //получаем путь к выбранному файлу
+                    string filePath = openFileDialog.FileName;
+
+                    try
+                    {   
+                        //читаем файл
+                        string content = File.ReadAllText(filePath);
+                        //заносим в текстбокс
+                        textBox1.Text = content;
+
+                    }
+                    catch ( Exception ex)
+                    {  
+
+                        MessageBox.Show($"ошибка: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void open_click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+      
+    }
+       
+    }
+
+
---
+++ Новая папка/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Journal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Journal")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3c31e4f0-838a-4558-8ca2-be9ef1a3382a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
