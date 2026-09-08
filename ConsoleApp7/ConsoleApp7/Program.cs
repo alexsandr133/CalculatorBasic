bcea0d2e095595b9ca6eae6949900fbfe365ed71
alexsandr133 <alexsandramg65@gmail.com>
2026-09-08 15:26:49 +0500

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вывл=одим первое число
+            Console.WriteLine("привествую ввидите первое число");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите имя");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("введите фамилию");
+            string famylya = Console.ReadLine();
+
+            Console.WriteLine("введите размер зарпоаты");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"привет пользователь: ваши данные:{age} возраст имя {name} фамилия {famylya} зарплата {salary}");
+
+        }
+    }
+}
