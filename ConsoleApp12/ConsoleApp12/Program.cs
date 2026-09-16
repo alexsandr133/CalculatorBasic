607199d40e08464fba3bda9aad41ac64f8883b0e
alexsandr133 <alexsandramg65@gmail.com>
2026-09-16 22:56:58 +0500

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//используем ресурсы visual basic
+using Microsoft.VisualBasic;
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //текстовая переменная
+            string name;
+            //отображение окна с поле ввода\
+            name = Interaction.InputBox(
+                "как вас зовут?", //текст над аолем ввода
+                "давай познакомися" //название окна
+                );
+            string txt = "очень приятно, "+name+"!";
+            //окно с сообщением
+            MessageBox.Show(txt, "знакомство состоялось");
+
+        }
+    }
+    
+}
