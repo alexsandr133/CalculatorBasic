8d0b13dd2f5fe80be2aa71ecfdcba47295435732
alexsandr133 <alexsandramg65@gmail.com>
2026-09-17 11:27:58 +0500

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //текстовая переменная
+            string name;
+            //заголовок консольного окна
+            Console.Title = "давайте познакомимя";
+            //сообщение в консоли
+            Console.WriteLine("Как вас зовут");
+            //считывание текста
+            name = Console.ReadLine();
+            //еще одна тестовая переменная
+            string txt = "очень приятно , "+name+"!";
+            //заголовок консольного окна
+            Console.Title = "знакомство состялось";
+            //сообщение в консоли
+            Console.WriteLine(txt);
+        }
+    }
+}
