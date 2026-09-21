3728b2bc0b21c81c80eb6749ac16515a2497591c
alexsandr133 <alexsandramg65@gmail.com>
2026-09-22 00:56:15 +0500

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//использлвание пространства имен
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+namespace ConsoleApp16
+{
+    
+    class EnteringInteger
+    {
+        static void main() {
+
+            //текстовая переменные
+            string res, txt;
+            //целочисденные переменные
+            int year = 2026, age, born;
+            //отображение окна с полем ввода
+            res = Interaction.InputBox("в каком году вы родились?", "год рождения");
+            //преобразование текста в число
+            born = Int32.Parse(res);
+            //вычисление возраста
+            age = year - born;
+            txt = "тогда вам " + age + "!";
+            //окно с сообщением
+            MessageBox.Show(txt, "возраст");
+
+         }
+    }
+}
