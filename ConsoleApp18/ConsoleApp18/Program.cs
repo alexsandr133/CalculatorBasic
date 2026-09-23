4f3f1d293c8982d346298dbb255fc1e7820112fa
alexsandr133 <alexsandramg65@gmail.com>
2026-09-23 08:22:44 +0500

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ConsoleApp18
+{
+    internal class DialogWindow
+    {
+        static void Main(string[] args)
+        {
+            //текстовая переменная
+            string name;
+            string surname;
+
+            name = Interaction.InputBox(
+            "как вас зовут?",
+            "давайте познакомимся"
+             );
+            //переменная с выводом
+            string text = "очень приятно было познакомится, " + name + "!";
+
+            //окно с сообщением
+            MessageBox.Show(text, "знакомство состоялось");
+        }
+    }
+}
