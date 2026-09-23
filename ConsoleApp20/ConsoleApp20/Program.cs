5072a2ae8d857150f7ee90460e37c0810a11551f
alexsandr133 <alexsandramg65@gmail.com>
2026-09-23 08:53:01 +0500

    Add files via upload

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class ConsoleDialog
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "в этом оконе вы вводите свое имя и фамилию";
+            Console.Write("введите имя и фамилию");
+            string name, surname;
+            name = Console.ReadLine();
+            surname = Console.ReadLine();
+            string txt = $"приятно познакомится пользователь,{name} {surname}";
+            Console.WriteLine(txt);
+        }
+    }
+}
