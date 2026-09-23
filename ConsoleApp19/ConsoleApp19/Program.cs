34f268680f8d9c31398d827bdc420e50c9e443ab
alexsandr133 <alexsandramg65@gmail.com>
2026-09-23 08:40:57 +0500

    Add files via upload

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ConsoleApp19
+{
+    internal class DemoWindow
+    {
+        static void Main(string[] args)
+        {
+            string name, surname ;
+            name = Interaction.InputBox(
+            "введите свои имя и фаммилию",
+            "в этом диалоговом окне вывкдктся вагше имя и фамилия"
+            );
+            string txt = "приятно познакоммится, "+name+"!";
+            MessageBox.Show("приятно было познакомится",txt);
+        }
+    }
+}
