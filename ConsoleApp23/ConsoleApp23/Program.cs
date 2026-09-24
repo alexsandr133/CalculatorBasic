ea4be5c3786955d1934dddf978b123ea3fbb0c56
michael_foxyman <alexsandramg65@gmail.com>
2026-09-25 00:47:20 +0500

    Add files via upload

---
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp23
+{
+    internal class ConsoleDialog
+    {
+        static void Main(string[] args)
+        {  
+            Console.Title = "приветствую вас пользователь";
+            Console.Write("введите имя");
+            string name = Console.ReadLine();
+            Console.Title = ("введите возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+            string txt = $"инофрмацмя о пользователе: {name},{age}";
+            Console.WriteLine(txt);
+            Console.Title = "знакомство соятоялось";
+
+
+        }
+    }
+}
