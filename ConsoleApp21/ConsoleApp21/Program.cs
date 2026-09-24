ef5efa2d45966bdeaf122d42092e9d84a0e4afe1
michael_foxyman <alexsandramg65@gmail.com>
2026-09-24 21:12:05 +0500

    Add files via upload

---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace ConsoleApp21
+{
+    internal class DialogWindow
+    {
+        static void Main(string[] args)
+        {
+            string res, txt;
+            int born,year = 2027, age ;
+
+            res = Interaction.InputBox(
+            "пользователь введите возраст"
+            );
+            born = Int32.Parse(res);
+            age = year - born;
+            txt = "тогда вам +" + age + " лет";
+            MessageBox.Show(txt);
+        }
+    }
+
+    
+}
