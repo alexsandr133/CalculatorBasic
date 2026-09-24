df28dd058000de3b1769d62cf41f6ddcb6464266
michael_foxyman <alexsandramg65@gmail.com>
2026-09-25 02:05:39 +0500

    Add files via upload

---
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monday = 1;
+            int thursday = 2;
+            int wensday = 3;
+            int chetverg = 4;
+            int friday = 5;
+            int satursday = 6;
+            int voskresene = 7;
+            string res, txt, name, name1;
+            int kolichestvo, count;
+            name = Interaction.InputBox(
+            "введите название дня недели",
+            "привествую вас пользователь"
+             );
+            name1 = Interaction.InputBox(
+            "введите название для месяца",
+            "привествую вас пользователь"
+
+            );
+            res = Interaction.InputBox(
+             "введите число"   
+            );
+            count = Int32.Parse(res);
+            if (name = 1)
+            {
+
+            }
+            txt = "инормация о дне недели"
+        }
+    }
+}
